7f11d34d862ddc81023353896dad053c43e57b3b
Alex Lewis <alexvlewis@gmail.com>
2021-07-02 13:42:43 -0700

    Update deploy script

--- scripts/deploy-nft.ts
+++ scripts/deploy-nft.ts
@@ -1,4 +1,6 @@
-import { ethers } from 'hardhat'
+import { ethers } from 'ethers'
+import { deployContract } from 'ethereum-waffle'
+import DAOfiV2Pair from '../build/contracts/DAOfiV2Pair.sol/DAOfiV2Pair.json'
 
 async function main() {
   const provider = new ethers.providers.JsonRpcProvider(
@@ -7,19 +9,21 @@ async function main() {
   const wallet = new ethers.Wallet(process.env.PRIVATE_KEY || '', provider)
   console.log('Wallet:', wallet.address)
 
-  const DAOfiV2Pair = await ethers.getContractFactory('DAOfiV2Pair')
-
-  const pair = await DAOfiV2Pair.deploy(
-    'Communifty Test NFT',
-    'TESTNFT',
-    'https://api.hodlink.io/4_20_21/',
-    process.env.PROXY || '0xf57b2c51ded3a29e6891aba85459d600256cf317',
-    wallet.address,
-    100, // tokens
-    1, // start x
-    1e6, // m
-    2, // n
-    50, // fee
+  const pair = await deployContract(
+    wallet,
+    DAOfiV2Pair,
+    [
+      'Communifty Test NFT',
+      'TESTNFT',
+      'https://api.hodlink.io/4_20_21/',
+      process.env.PROXY || '0xf57b2c51ded3a29e6891aba85459d600256cf317',
+      wallet.address,
+      100, // tokens
+      1, // start x
+      1e6, // m
+      2, // n
+      50, // fee
+    ],
     {
       gasLimit: 8000000,
       gasPrice: ethers.utils.parseUnits('20', 'gwei'),
