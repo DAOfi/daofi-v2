4197daf60837ddc169694a1f42fd5fee138b8184
Alex Lewis <alexvlewis@gmail.com>
2021-07-19 16:57:29 -0700

    Script to generate thumbs from video

---
+++ scripts/thumbs.sh
@@ -0,0 +1,7 @@
+readarray -d '' entries < <(printf '%s\0' *.mp4 | sort -zV)
+i=1
+for entry in "${entries[@]}"; do
+  # do something with $entry
+  ffmpeg -ss 0.5 -i $i.mp4 -vframes 1 -s 400x400 -f image2 $i.jpg
+  ((i=i+1))
+done
