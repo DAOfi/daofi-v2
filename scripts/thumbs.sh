6f25b91bd3cdb8a1dd3526e48248963c1b1fc2c5
Alex Lewis <alexvlewis@gmail.com>
2021-07-19 17:51:04 -0700

    Use mjpeg pixel format

--- scripts/thumbs.sh
+++ scripts/thumbs.sh
@@ -2,6 +2,6 @@ readarray -d '' entries < <(printf '%s\0' *.mp4 | sort -zV)
 i=1
 for entry in "${entries[@]}"; do
   # do something with $entry
-  ffmpeg -ss 0.5 -i $i.mp4 -vframes 1 -s 400x400 -f image2 $i.jpg
+  ffmpeg -ss 0.5 -i $i.mp4 -vframes 1 -s 400x400 -f mjpeg $i.jpg
   ((i=i+1))
 done
