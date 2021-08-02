682bdc1e250d14e128ad321e5a8bfd6de4db4228
Alex Lewis <alexvlewis@gmail.com>
2021-08-02 12:07:03 -0700

    Update thumbnail script

--- scripts/thumbs.sh
+++ scripts/thumbs.sh
@@ -2,6 +2,6 @@ readarray -d '' entries < <(printf '%s\0' *.mp4 | sort -zV)
 i=1
 for entry in "${entries[@]}"; do
   # do something with $entry
-  ffmpeg -ss 0.5 -i $i.mp4 -vframes 1 -s 400x400 -f mjpeg $i.jpg
+  ffmpeg -i $i.mp4 -frames:v 1 -s 400x400 $i.jpg
   ((i=i+1))
 done
